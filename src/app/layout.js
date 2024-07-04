5bae8df174d6357c6faee445b9105d2ea0d9cc45
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-07-05 04:08:07 +0600

    renew next install

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/shared/Navbar/Navbar";
+import Footer from "@/shared/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme={"mytheme"}>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
