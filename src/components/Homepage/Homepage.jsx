f5fda07b4547cd2858aebf8a0d54a24645fc84c8
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-07-07 01:42:56 +0600

    about section complete

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
 import About from './About';
+import Services from './Services';
+import Footer from '@/shared/Footer/Footer';
 
 const Homepage = () => {
     return (
         <div>
             <Banner />
             <About />
+            <Services />
+            <Footer />
         </div>
     );
 };
